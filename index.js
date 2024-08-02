c353a60acc1470d2a652dd0ed826c6a0a1dcfa84
vitoriaacaroline <134931502+vitoriaacaroline@users.noreply.github.com>
2024-08-02 11:44:58 -0300

    aula01

---
+++ index.js
@@ -0,0 +1,12 @@
+ const express = require('express');
+
+ const app = express();
+ 
+ // const porta = process.env.PORT || 3333;
+ const porta = 3333;
+
+ app.listen(porta, () => {
+    console.log('Servidor iniciado na porta' + porta);
+    //console.log('Servidor iniciado na porta ${porta}`);
+ });
+ 
