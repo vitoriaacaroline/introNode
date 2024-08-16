d9a63a2868e7e3a7361e601572dcd8d5fc147838
vitoriaacaroline <134931502+vitoriaacaroline@users.noreply.github.com>
2024-08-16 11:34:05 -0300

    aula03

---
+++ routes/route.js
@@ -0,0 +1,16 @@
+const express = require('express'); 
+const router = express.Router(); 
+
+// referência a controllers que serão utilizados nas rotas
+const UsuariosController = require('../controllers/usuarios'); 
+
+// definição das rotas
+router.get('/usuarios', UsuariosController.listarUsuarios); 
+router.post('/usuarios', UsuariosController.cadastrarUsuarios); 
+router.patch('/usuarios', UsuariosController.editarUsuarios); 
+router.delete('/usuarios', UsuariosController.apagarUsuarios); 
+// post
+// patch
+// delete
+
+module.exports = router;
